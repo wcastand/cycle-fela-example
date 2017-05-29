2f86faf2c289d41e9be21e98e55e03c327834bb8
wcastand <wcastand@gmail.com>
2017-05-29 13:52:33 +0200

    update cycle-fela and add composition for createComponent

--- src/app.js
+++ src/app.js
@@ -2,15 +2,17 @@ import {div, input} from '@cycle/dom'
 import { createComponent } from 'cycle-fela'
 import xs from 'xstream'
 
-const Title = createComponent(({mobile = false}) => ({color: mobile ? "red" : "yellow"}), 'h1')
-
+const Bold = () => ({fontWeight: 'bold'})
+const Title = createComponent(({mobile = false}) => ({fontWeight: 'normal', color: mobile ? "red" : "gray"}), 'h1')
+const BoldTitle = createComponent(Bold, Title)
 export function App (sources) {
   const vtree$ = sources.DOM.select('input').events('click')
   .map(ev => ev.target.checked)
       .startWith(false)
       .map(toggled => div([
         input({attrs: {type: 'checkbox'}}),
-        Title({mobile: toggled}, 'Hello World')
+        Title({mobile: toggled}, 'Hello World'),
+        BoldTitle({mobile: toggled}, 'Hello World'),
       ])
   )
   const sinks = {
